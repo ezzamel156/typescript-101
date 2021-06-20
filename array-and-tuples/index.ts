4953d70af852f28f0d84ac3d75fc3b9c012204e1
ezzamel <ezzamel156@gmail.com>
2021-06-20 17:17:17 +0800

    Finished array, type

---
+++ array-and-tuples/index.ts
@@ -0,0 +1,17 @@
+const arsenalPlayers: string[] = [
+    'Ozil', 
+    'Giroud', 
+    'Ramsey', 
+    'Walcott'
+];
+
+const arsenalJersey: number[] = [10,12,8,14];
+
+const playerOrJersey: (string|number)[] = [
+    'ozil',
+    10,
+    'Walcott',
+    14
+];
+
+const tuple: [number, string, Date] = [14, 'Walcott', new Date];
